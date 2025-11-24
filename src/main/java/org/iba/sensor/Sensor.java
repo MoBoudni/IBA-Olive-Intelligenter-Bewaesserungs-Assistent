8884c46b199d4dffa5359bdcc146e6e8bf73fc91
MoBoudni <168630329+MoBoudni@users.noreply.github.com>
2025-11-24 22:24:20 +0100

    Refactor Sensor class to an interface
    Changed Sensor class to an interface for better extensibility.

--- src/main/java/org/iba/sensor/Sensor.java
+++ src/main/java/org/iba/sensor/Sensor.java
@@ -1,4 +1,14 @@
 package org.iba.sensor;
 
-public class Sensor {
+/**
+ * Definiert das allgemeine Interface für Sensoren im IBA-Olive System (MVP+)
+ * dies ermöglicht eine spätere einfache Erweiterung um reale Sensordaten.
+ */
+public interface Sensor {
+
+    /**
+     * Liest den aktuellen Messwert des Sensors.
+     * @return Der gemessene Wert (z.B. Bodenfeuchte in Prozent).
+     */
+    double messWertLesen();
 }
